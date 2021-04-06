88b57c87a144e4e867d04f75b90c0030f380f924
Luca Braglia <lbraglia@gmail.com>
2021-04-06 18:07:07 +0200

    update

--- misc/test.R
+++ misc/test.R
@@ -7,15 +7,15 @@ sandbox_f      <- "/tmp/assign_sandbox"
 rev1_sandbox_f <- "/tmp/assign_rev1_sandbox"
 translate_f    <- "/tmp/assign_translate"
 revise2_f      <- "/tmp/assign_revise2"
-prj <- lbav2::prj$new(id = prj_id, yt_id = prj_yt_id);
-
 ## setup del progetto: bisogna proprio volerlo :)
 if (FALSE){
     system('rm -rf subs/test')
-    prj$setup()
+    lbav2::prj$new(id = prj_id, yt_id = prj_yt_id)$setup()
 }
 
+
 ## create_sandbox
+prj <- lbav2::prj$new(id = prj_id, yt_id = prj_yt_id)
 trn <- c(prj$users$translators(), prj$users$revisors2())
 rev1 <- c(prj$users$translators(), prj$users$revisors2())
 write_lines(trn, f = sandbox_f)
@@ -23,10 +23,17 @@ write_lines(rev1, f = rev1_sandbox_f)
 prj$create_sandbox(sandbox_f = sandbox_f, rev1_sandbox_f = rev1_sandbox_f)
 
 ## assign translate
+prj <- lbav2::prj$new(id = prj_id, yt_id = prj_yt_id)
 write_lines(trn,  f = translate_f)
 prj$assign(translate_f = translate_f)
+prj$monitoring()
+
+## mark avanzamento
+prj <- lbav2::prj$new(id = prj_id, yt_id = prj_yt_id)
+
 
 ## assign revision2
+prj <- lbav2::prj$new(id = prj_id, yt_id = prj_yt_id)
 write_lines(rev1, f = revise2_f)
 prj$assign(revise2_f = revise2_f)
-
+prj$monitoring()
