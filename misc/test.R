a91a4fe2c3712693cb233f46e64d36e3c0075448
Luca Braglia <lbraglia@gmail.com>
2021-04-06 09:27:23 +0200

    update

--- misc/test.R
+++ misc/test.R
@@ -3,8 +3,10 @@ library(lbmisc)
 devtools::load_all("~/src/rpkg/lbav2")
 prj_id <- 'test'
 prj_yt_id <- 'Lox6tAor5Xo'
-sandbox_f <- "/tmp/assign_sandbox"
+sandbox_f      <- "/tmp/assign_sandbox"
 rev1_sandbox_f <- "/tmp/assign_rev1_sandbox"
+translate_f    <- "/tmp/assign_translate"
+revise2_f      <- "/tmp/assign_revise2"
 prj <- lbav2::prj$new(id = prj_id, yt_id = prj_yt_id);
 
 ## setup del progetto: bisogna proprio volerlo :)
@@ -20,5 +22,11 @@ write_lines(trn, f = sandbox_f)
 write_lines(rev1, f = rev1_sandbox_f)
 prj$create_sandbox(sandbox_f = sandbox_f, rev1_sandbox_f = rev1_sandbox_f)
 
-## srt
+## assign translate
+write_lines(trn,  f = translate_f)
+prj$assign(translate_f = translate_f)
+
+## assign revision2
+write_lines(rev1, f = revise2_f)
+prj$assign(revise2_f = revise2_f)
 
