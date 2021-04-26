2f2333f33a26c9eb9bf63b63a1080d78ac3658c6
Luca Braglia <lbraglia@gmail.com>
2021-04-26 12:26:15 +0200

    update

--- projects/gymix.R
+++ projects/gymix.R
@@ -1,11 +1,3 @@
 ## devtools::load_all("~/src/rpkg/lbav2")
 prj <- lbav2::prj$new(id = 'gymix', yt_id = 'lw53nODhRXU')
 prj$menu()
-
-
-if (FALSE){
-
-subs/sandbox/Agn25_trn.srt
-subs/gymix/subs_000500_eleonoragee.srt
-    
-}
